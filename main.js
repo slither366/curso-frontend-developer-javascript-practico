902a477c51afd41c2ec4250ab4d73f8120ff82fd
David Flores <slither366@gmail.com>
2022-10-27 22:30:29 -0500

    Agregando interacción con el menu hamburguesa aparece y desaparece de forma responsiva gracias al media query

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamburguesa = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburguesa.addEventListener('click', toggleMobileMenu);
+
 
 function toggleDesktopMenu(){
-    console.log('click');
+    //console.log('click');
     desktopMenu.classList.toggle('inactive');//si ya tiene clase la quita y si no la tiene la agrega
 }
 
-console.log('Js console log');
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
+
+//console.log('Js console log');
