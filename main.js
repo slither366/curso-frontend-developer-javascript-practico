8e448e6954e236a43c34f8c5ce63daeabceaf4dd
David Flores <slither366@gmail.com>
2022-10-31 03:24:52 -0500

    Se maqueto con Javascript los elementos del carrito de compras, se agrego en un Arraylist y un for para iterarlos

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const menuHamburguesa = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamburguesa.addEventListener('click', toggleMobileMenu);
@@ -41,3 +42,104 @@ function toggleMenuCarrito(){
 }
 
 //console.log('Js console log');
+
+const productList = [];
+
+productList.push({
+    name: 'Bike',
+    price: 20,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+productList.push({
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+productList.push({
+    name: 'Radio',
+    price: 150,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+productList.push({
+    name: 'Computer',
+    price: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+productList.push({
+    name: 'Laptop',
+    price: 1220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+productList.push({
+    name: 'Keyboard',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+productList.push({
+    name: 'Mouse',
+    price: 40,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+productList.push({
+    name: 'Cooler',
+    price: 230,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+/*
+<div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+<div class="product-info">
+  <div>
+    <p>$120,00</p>
+    <p>Bike</p>
+  </div>
+  <figure>
+    <img src="./icons/bt_add_to_cart.svg" alt="">
+  </figure>
+</div>
+</div>
+*/
+
+function renderProducts(arr){
+    for (product of arr){
+
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const imgCard = document.createElement('img');
+        imgCard.setAttribute('src',product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const divVacio = document.createElement('div');
+        const parrafoPrice = document.createElement('p');
+        parrafoPrice.innerText ='$'+product.price;
+        const parrafoName = document.createElement('p');
+        parrafoName.innerText = product.name;
+
+        const figure = document.createElement('figure');
+        const imagen2 = document.createElement('img');
+        imagen2.setAttribute('src','./icons/bt_add_to_cart.svg');
+        
+        figure.appendChild(imagen2);
+        divVacio.appendChild(parrafoPrice);
+        divVacio.appendChild(parrafoName);
+        productInfo.appendChild(divVacio);
+        productInfo.appendChild(figure);
+        productCard.appendChild(imgCard);
+        productCard.appendChild(productInfo);
+        cardsContainer.appendChild(productCard);
+        
+    }
+}
+
+renderProducts(productList);
